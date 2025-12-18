7d13f053fd013c1e2a35487a23e5df594f72cd4b
Anithavk <anithaavk@gmail.com>
2025-12-18 23:17:09 +0530

    s commit

--- src/components/FilterDropdown.jsx
+++ src/components/FilterDropdown.jsx
@@ -1,10 +1,28 @@
-export default function FilterDropdown({ onChange }) {
-return (
-<select onChange={(e) => onChange(e.target.value)} className="border p-2">
-<option value="">All</option>
-<option value="movie">Movie</option>
-<option value="series">Series</option>
-<option value="episode">Episode</option>
-</select>
-);
+export default function FilterDropdown({ value, onChange }) {
+  return (
+    <select
+      value={value}
+      onChange={(e) => onChange(e.target.value)}
+      className="
+        bg-gray-900 text-white
+        border border-gray-600
+        px-3 py-2 rounded
+        focus:outline-none focus:ring-2 focus:ring-blue-500
+        hover:border-gray-400
+      "
+    >
+      <option value="" className="bg-white text-black">
+        All
+      </option>
+      <option value="movie" className="bg-white text-black">
+        Movie
+      </option>
+      <option value="series" className="bg-white text-black">
+        Series
+      </option>
+      <option value="episode" className="bg-white text-black">
+        Episode
+      </option>
+    </select>
+  );
 }
